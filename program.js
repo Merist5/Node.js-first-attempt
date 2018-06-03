9c5b708010c96f0262206fe7d9f4aa1d0a2726f5
Joanna Urbiak <joanna.urbiak@o2.pl>
2018-06-03 22:17:03 +0200

    Add program.js

---
+++ program.js
@@ -0,0 +1,20 @@
+process.stdin.setEncoding('utf-8');
+process.stdin.on('readable', function() {
+    var input = process.stdin.read();
+    if (input !== null) {
+        var instruction = input.toString().trim();
+        switch(instruction) {
+            case '/exit':
+                process.stdout.write('Quitting app!\n');
+                process.exit();
+                break;
+            case '/version':
+                process.stdout.write('Node version:\n' + process.versions.node
+                + ' \n');
+                process.stdout.write('Language:\n' + process.env.LANG);
+                break;
+            default:
+                process.stderr.write('Wrong instruction!\n');
+        };
+    }
+});
